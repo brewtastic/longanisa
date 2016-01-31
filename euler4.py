419060f30090d0235e8a2e62021f8570ad331b12
bigmac <nathan.mclaughlin@gmail.com>
2016-01-31 15:17:35 -0800

    First commit

--- euler4.py
+++ euler4.py
@@ -1,4 +1,12 @@
 def palindrome(num):
     return str(num) == str(num)[::-1]
 
-while
+total = 0
+
+for i in xrange(99,999):
+    for l in xrange(99,999):
+        big = i * l
+        if palindrome(big):
+            if big > total:
+                total = big
+print total
---
+++ euler5.py
@@ -0,0 +1,6 @@
+from fractions import gcd
+from functools import reduce
+
+def lcm(a,b):
+    return a*b//gcd(a,b)
+print reduce(lcm, range(1,20+1))
